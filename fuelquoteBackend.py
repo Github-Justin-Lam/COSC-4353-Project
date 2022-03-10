a34eb45f7fc4bc5be3fb320b59aa41e881f74b3a
Justin Lam <wkjustinlam@gmail.com>
2022-03-10 02:42:49 -0600

    Update fuelquoteBackend.py

--- fuelquoteBackend.py
+++ fuelquoteBackend.py
@@ -44,6 +44,3 @@ def my_form_post():
         error_statement = "Fuel Quote Submitted!"
         print(request.form)
         return render_template('fuelquote.html', error=error_statement)
-
-if __name__ == '__main__':
-    app.run(debug=True)
